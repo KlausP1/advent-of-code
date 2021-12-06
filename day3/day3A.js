7e9942ac14f313f66657f9f8c60d3c1df0449216
KlausP1 <112825@emmauscollege.nl>
2021-12-06 08:10:12 +0000

    einde les ma 6-12

--- day3/day3A.js
+++ day3/day3A.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const readline = require('readline')
 
+let lines = [];
 let gamma = ""
 let epsilon = ''
 num1 = 0
 num0 = 0
 // total 11
 amount = 0
-ans = ''
 
 const rl = readline.createInterface({
     input: fs.createReadStream('day2/inputday2.txt'),
@@ -16,30 +16,26 @@ const rl = readline.createInterface({
 
 
 rl.on('line',(line) => {
-    if(line[amount] == '1'){
-        num1++
-    } else {
-        num0++
-    }
-    ans += '1'
+    lines.push(line);
 })
 
 rl.on('close', () => {
-    if(amount < 11){
-        // if(num0 > num1){
-        //     gamma += '0'
-        //     epsilon += "1"
-        // } else {
-        //     gamma += "1"
-        //     epsilon += "0"
-        //  }
-        // num1 = 0
-        // num0 = 0
+   for(let i = 0;i < lines.length;i++){
+   } 
+
+    if(amount < 12){
+        if(num0 > num1){
+            gamma += '0'
+            epsilon += "1"
+        } else {
+            gamma += "1"
+            epsilon += "0"
+         }
+        num1 = 0
+        num0 = 0
         amount++
         
     }
-    console.log(ans)
     console.log(amount)
     console.log(gamma)
-    console.log(epsilon)
 })
