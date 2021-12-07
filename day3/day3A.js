f6a4c8534edb500b43542d73065037110dbd0a02
KlausP1 <112825@emmauscollege.nl>
2021-12-07 10:47:59 +0000

    tussentest

--- day3/day3A.js
+++ day3/day3A.js
@@ -17,25 +17,45 @@ const rl = readline.createInterface({
 
 rl.on('line',(line) => {
     lines.push(line);
+
 })
 
-rl.on('close', () => {
-   for(let i = 0;i < lines.length;i++){
-   } 
 
-    if(amount < 12){
-        if(num0 > num1){
-            gamma += '0'
-            epsilon += "1"
-        } else {
-            gamma += "1"
-            epsilon += "0"
-         }
-        num1 = 0
+
+rl.on('close', () => {
+    for(i = 0;i<11;i++){
+        for(let j = 0;j < lines.length;j++){
+            //console.log(lines[j][i])
+            if(lines[j][i] == "1"){
+                num0++
+            }else {
+                num1++
+            }
+        }
+        if(num0>num1){
+            gamma+="0"
+            espilon+="1"
+        } else{
+            gamma+="1"
+            epsilon+="0"
+        }
         num0 = 0
-        amount++
-        
+        num1=0
     }
+
+    // if(amount < 12){
+    //     if(num0 > num1){
+    //         gamma += '0'
+    //         epsilon += "1"
+    //     } else {
+    //         gamma += "1"
+    //         epsilon += "0"
+    //      }
+    //     num1 = 0
+    //     num0 = 0
+    //     amount++
+        
+    // }
     console.log(amount)
     console.log(gamma)
 })
